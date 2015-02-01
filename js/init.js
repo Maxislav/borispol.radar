01116be8b7c9da9cfc07d35a667b5fe57f0b18b7
Maxislav <lmaxim@mail.ru>
2015-02-01 14:02:16 +0200

    Merge branch 'master' into res
    Conflicts:
    	build/scripts.min.js
    	index.html
    	js/app.js
    	js/init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ var brr;
 require.config({
 	waitSeconds: 60,
 	baseUrl: '',
-    urlArgs: "bust=2015.1.18-17:9:32",
+    urlArgs: "bust=2015.2.1-14:0:37",
 	paths: {
 		jquery: 'lib/jquery/jquery-min',
 		app: 'js/app',
