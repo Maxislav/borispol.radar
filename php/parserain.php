07a4865fb9a0a1fa8511a4a5d885adba06592bee
Maxislav <lmaxim@mail.ru>
2015-07-27 23:24:28 +0300

    parseimg

--- php/parserain.php
+++ php/parserain.php
@@ -11,65 +11,66 @@ list($width, $height, $type, $attr) = $size;
 
 
 $im = imagecreatefrompng($url);
-$rgb = imagecolorat($im, 10, 15);
+//$rgb = imagecolorat($im, 10, 15);
+$kiev = array(
+    "x"=> 211,
+    "y"=>231
+);
+$scaleFactor = 400/468;
 
-$colors = imagecolorsforindex($im, $rgb);
+$arrayDist = array();
+for($x = 0; $x<500; $x++){
+    for($y=0; $y<479; $y++){
+        $rgb = imagecolorat($im, $x, $y);
+        $colors = imagecolorsforindex($im, $rgb);
+        if($colors["red"]!=$colors["green"]){
+           // echo $colors["red"]." ".$colors["green"]." ".$colors["blue"]." xy: ";
+           // echo $x." ".$y."";
+           $distPx = sqrt(pow($x - $kiev["x"], 2) + pow($y - $kiev["y"], 2));
+           //$distPx = sqrt(pow(10, 2) + pow(10, 2));
+            $dist = $distPx*$scaleFactor;
+            //echo " distance ".$dist ."</br>";
+            /*array_push($arrayDist, array(
+                $dist => $colors["red"]." ".$colors["green"]." ".$colors["blue"]
+            ));*/
+           // array_push($arrayDist, ("a"=>$dist));
 
-echo $colors["red"];
-//var_dump($colors);
-
-
-
-
-if($image) {
-
-    $im = new Imagick();
-
-    $im->readImageBlob($image);
-    $size = getimagesize($im);
-    echo  $size;
-
-
-}
-
-$x = 1;
-$y = 1;
-$pixel = $image->getImagePixelColor($x, $y);
-
-$colors = $pixel->getColor();
-
-echo  $colors;
-
-$path ='../img/sat/'. date("YmdH").'00.gif';
-
-$file = fopen($path,'w') or die('Could not create file: ' . $path);
-chmod($fileName, 0777);
-fclose($file);
-
-file_put_contents($path, file_get_contents($url));
-
-$dir = '../img/sat/';
-
-$f = scandir($dir);
-$arrayImgs = array();
-
-foreach ($f as $file){
-    if(preg_match('/\.(gif)/', $file)){
-        array_push($arrayImgs, $file);
-       // echo $file.'<br/>';
+            //$arrayDist[$dist] =  $colors["red"]." ".$colors["green"]." ".$colors["blue"];
+            $arrayDist[$dist] = array(
+                "color"=>$colors["red"]." ".$colors["green"]." ".$colors["blue"],
+                "dist"=>$dist
+            );
+        }
     }
 }
-echo dirname(__FILE__).'/'.'<br/>';
-$delSplice = count($arrayImgs)-24;
-if(0 < $delSplice){
-    array_splice($arrayImgs,$delSplice);
 
-    foreach($arrayImgs as $file){
-        $path = dirname(__FILE__).'/'.$dir.$file;
-        unlink($path);
-        print_r(error_get_last());
-        echo $path.'<br/>';
+
+ksort($arrayDist);
+$sortArr = array();
+
+$type = array();
+
+foreach ($arrayDist as $key => $val) {
+   // echo "$key = $val</br>";
+   // echo "$key = ";
+
+    /*foreach ($val as $k => $v){
+        echo "$k = $v ";
+    }*/
+
+  // $type[$val["color"]]
+
+    array_push($sortArr,$val );
+   // echo "dist = " .$val["dist"]. " color = ". $val["color"];
+   // echo "</br>";
+}
+echo "</br>";
+
+foreach($sortArr as $key=>$val){
+    if(!$type[$va["color"]]){
+        $type[$va["color"]] = $val;
     }
-
 }
 
+//echo json_encode($sortArr[0]);
+echo json_encode($sortArr);
