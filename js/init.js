2eff8f407e98e6193232865c4081bf8359d5ee10
Maxim Lipatov <lmaxim@mail.ru>
2016-10-26 20:58:49 +0300

    load texture ok

--- js/init.js
+++ js/init.js
@@ -29,7 +29,7 @@ Date.prototype.secondsFromStartDay = function () {
 require.config({
     waitSeconds: 60,
     baseUrl: '',
-    urlArgs: "bust=07.10.2016-13:52:34",
+    urlArgs: "bust=26.10.2016-20:53:40",
     paths: {
         jquery: 'lib/jquery/jquery-1.12.4',
         app: 'js/app',
