ffa22c2f2c0c8997c88e8bf2640c109868f2d3c6
Maxim Lipatov <lmaxim@mail.ru>
2016-11-03 18:17:30 +0200

    new earth

--- php/SatelliteImages.php
+++ php/SatelliteImages.php
@@ -7,13 +7,14 @@
 //echo "http://www.metoffice.gov.uk/public/data/CoreProductCache/SatelliteImages/ItemList/Composite/World";
 ///2016-10-28T14:00:00Z
 date_default_timezone_set('UTC');
-$dateStart = mktime(date("H")-2, 0, 0, date("m")  , date("d"), date("Y"));
+$dateStart = mktime(date("H")-4, 0, 0, date("m")  , date("d"), date("Y"));
 $dateEnd = mktime(date("H")-1, 0, 0, date("m")  , date("d"), date("Y"));
 $url = "http://www.metoffice.gov.uk/public/data/CoreProductCache/SatelliteImages/ItemList/Composite/World/".date("Y-m-d\TH:i:s", $dateStart)."Z"."/".date("Y-m-d\TH:i:s", $dateEnd)."Z";
 $xmlstr = file_get_contents($url);
 $ProductList = new SimpleXMLElement($xmlstr);
 
 $urlImg = $ProductList->Product->ProductURI;
+//echo $url;
 header('Content-type: image/jpeg');
 echo file_get_contents($urlImg);
 ?>
