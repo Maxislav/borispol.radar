c0e70280ebc3c244f0770f8df0d94a34f1c41fe7
Maxim Lipatov <lmaxim@mail.ru>
2016-12-14 14:32:18 +0200

    getImage file create

---
+++ module/ymetrika/permits.js
@@ -0,0 +1,9 @@
+define(function () {
+  return{
+    id:23847604,
+    allow: [
+      'borispol',
+      'senko'
+    ]
+  }
+});
--- module/ymetrika/ymetrika.js
+++ module/ymetrika/ymetrika.js
@@ -1,56 +1,59 @@
 /**
  * Created by mars on 12.01.16.
  */
-(function (d, w, c) {
-//http://borispol.hol.es
-    if (!/borispol/.test(window.location.origin)){
-        return
+
+define(['module/ymetrika/permits.js'], function (permits) {
+  (function (d, w, c) {
+    const rex =  RegExp(permits.allow.join('|'));
+    if (! window.location.origin.match(rex)){
+      return
     }
 
     require(['text!' + 'module/ymetrika/ymetrika.html'], function (html) {
-        var div = $(html);
-        document.body.appendChild(div[0]);
+      var div = $(html);
+      document.body.appendChild(div[0]);
 
 
-       var dNscr =   document
-             .createElement('noscript')
-             .appendChild(document
-                 .createElement('div'));
-           div.append(dNscr);
+      var dNscr =   document
+        .createElement('noscript')
+        .appendChild(document
+          .createElement('div'));
+      div.append(dNscr);
 
 
-        var imgYm = new Image();
-        imgYm.src = "//mc.yandex.ru/watch/23847604";
-        imgYm.style.position = 'absolute';
-        imgYm.style.left = '-9999px';
-        dNscr.appendChild(imgYm);
+      var imgYm = new Image();
+      imgYm.src = "//mc.yandex.ru/watch/"+permits.id;
+      imgYm.style.position = 'absolute';
+      imgYm.style.left = '-9999px';
+      dNscr.appendChild(imgYm);
 
-        (w[c] = w[c] || []).push(function () {
-            try {
-                w.yaCounter23847604 = new Ya.Metrika({id: 23847604,
-                    clickmap: true,
-                    trackLinks: true,
-                    accurateTrackBounce: true,
-                    trackHash: true
-                });
-            } catch (e) {
-            }
-        });
-
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () {
-                n.parentNode.insertBefore(s, n);
-            };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else {
-            f();
+      (w[c] = w[c] || []).push(function () {
+        try {
+          w.yaCounter23847604 = new Ya.Metrika({id: permits.id,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            trackHash: true
+          });
+        } catch (e) {
         }
+      });
+
+      var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () {
+          n.parentNode.insertBefore(s, n);
+        };
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+      if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+      } else {
+        f();
+      }
     })
 
-})(document, window, "yandex_metrika_callbacks");
+  })(document, window, "yandex_metrika_callbacks");
+})
